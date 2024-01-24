684189f731474b3b362991c7703f2f7d9139b764
kille250 <Dionyssioss@gmail.com>
2024-01-24 22:15:52 +0100

    Update JoinNow.js

--- src/components/JoinNow.js
+++ src/components/JoinNow.js
@@ -8,10 +8,10 @@ const JoinNow = () => {
           <h3>Join us now!</h3>
         </div>
         <div className="joinUs-para p-color">
-          Invite our bot to your Discord server!{" "}
+          Invite Solidbooru-AI to your Discord server!{" "}
         </div>
         <button className="btn">
-          <Link className="h-color no-decoration" href="#">
+          <Link className="h-color no-decoration" href="https://discord.com/invite/aFsdveRv2R">
             Invite now!
           </Link>
         </button>
