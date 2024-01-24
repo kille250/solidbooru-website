4ff770d2b7f1b9c5bfef5a479ac17207d7a87312
kille250 <Dionyssioss@gmail.com>
2024-01-24 22:12:25 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -57,28 +57,9 @@ const Footer = () => {
         </div>
         <div className="footer-sec2">
           <hr />
-          <div>
-            <p className="p-color">
-              Created with ❤️ by{" "}
-              <Link
-                href="https://github.com/preetsuthar17"
-                className="h-color no-decoration"
-              >
-                Preet Suthar
-              </Link>
-            </p>
-            <p className="p-color">{year} &copy; All rights reserved!</p>
-          </div>
           <div className="footer-sec2-link">
             <Link className="p-color no-decoration" href="#">
               /support
-            </Link>
-            <Link
-              target="_blank"
-              className="p-color no-decoration"
-              href="https://github.com/preetsuthar17/discord-bot-website-template"
-            >
-              /source-code
             </Link>
             <Link className="p-color no-decoration" href="#">
               /privacy
