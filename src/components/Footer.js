e61c6472df2e3cc5273e68ccedfee992cb0587bc
kille250 <Dionyssioss@gmail.com>
2024-01-24 22:50:44 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
             <ul>
               <li>
                 <Link className="h-color" href="/">
-                  bot.gg
+                  solidbooru.online
                 </Link>
                 <span
                   style={{
